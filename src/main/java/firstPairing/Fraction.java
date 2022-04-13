c7325b0b79766011a956e0dce4a5ed501d94184a
dylanA505 <dylana@mobileintegration-group.com>
2022-04-13 15:22:51 -0700

    added a new String

--- src/main/java/firstPairing/Fraction.java
+++ src/main/java/firstPairing/Fraction.java
@@ -13,6 +13,7 @@ public final class Fraction extends Number implements Comparable<Fraction> {
 
     private final long numerator;
     private final long denominator;
+    private String dylan;
 
 
     /**
